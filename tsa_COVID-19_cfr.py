3e61575ca426005e49d19b51fc78063142516dcc
Prof. Marco Arieli Herrera-Valdez <marcoh@ciencias.unam.mx>
2020-04-09 07:33:14 -0500

    Add files via upload
    Time series analysis for case fatality ratios for COVID-19. Data source: https://github.com/CSSEGISandData/COVID-19/

--- tsa_COVID-19_cfr.py
+++ tsa_COVID-19_cfr.py
@@ -1,10 +1,5 @@
 # COVID-19_cfr_Jan-March_2020
-
-import pandas as pd
-import numpy as np
-import matplotlib.pylab as gr
-from matplotlib.dates import mdates
-import datetime
+from tsam_COVID19_baseCode import *
 
 ref="""Data obtained from https://github.com/CSSEGISandData/COVID-19/"""
 
@@ -18,6 +13,17 @@ if urlData==1:
     urlRecov = srcDir+'time_series_covid19_recovered_global.csv'
     recov = pd.read_csv(urlRecov,index_col=None)
 
+if 1:
+    urlTests='https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/testing/'
+    testsF='covid-testing-all-observations.csv'
+    tests = pd.read_csv(urlTests+testsF,index_col=None)
+
+#world population Data
+if 1:
+    urlWorldPop='https://stats.oecd.org/sdmx-json/data/DP_LIVE/.POP.../OECD?contentType=csv&detail=code&separator=comma&csv-lang=en'
+    worldPops = pd.read_csv(urlWorldPop,index_col=None)
+
+
 # From local directory
 localData=0
 if localData==1:
@@ -28,6 +34,7 @@ if localData==1:
     cases=pd.read_csv(srcDir+casesF)
     deaths=pd.read_csv(srcDir+deathsF)
     recov=pd.read_csv(srcDir+recovF)
+
 # ------------------------------
 # Description of the data so that the headers and the columns
 # without case data are distinguished
@@ -38,11 +45,10 @@ nHeaderRows=1;
 #
 nHeaderCols=3
 # how to generate date lists from a baseline using the datetime
-#base = datetime.datetime(2020, 1, 22)
-#dates = np.array([base + datetime.timedelta(days=i) for i in range(nDays)])
 dates = cases.columns[4:]
 nDays = len(dates)
-print('Dates:\n',dates)
+print('Got data from %d Dates:\n'%(len(dates)))
+print(dates)
 
 # -------------------
 print("""Data curation
@@ -57,14 +63,8 @@ npCases = cases.to_numpy()
 countries = np.unique(npCases[:,1])
 nCountries = len(countries)
 # -------------------
-# Ordering data to gather same country data
+# Gather same country data
 # -------------------
-def sortDataByCountry(df,nHeaderCols=4):
-    iCoun= df.iloc[:,1].to_numpy().argsort()
-    x = df.iloc[iCoun,nHeaderCols:].to_numpy()
-    places = df.iloc[iCoun,1].to_numpy()
-    return x, places
-
 gCases, countries_Cases= sortDataByCountry(cases, nHeaderCols)
 gDeaths,countries_Deaths = sortDataByCountry(deaths, nHeaderCols)
 gRecov,countries_Recov = sortDataByCountry(recov, nHeaderCols)
@@ -72,143 +72,97 @@ gRecov,countries_Recov = sortDataByCountry(recov, nHeaderCols)
 # -------------------
 # Sum the counts from each country and construct a new array
 # -------------------
-def gatherDataByCountry(df,nHeaderCols=4):
-    cc = df.iloc[:,1].to_numpy()
-    countries = np.unique(df.iloc[:,1].to_numpy())
-    nCountries = len(countries);
-    x = list()
-    for n in range(nCountries):
-        iC= np.where(cc== countries[n])[0]
-        a =df.iloc[iC,nHeaderCols:].to_numpy()
-        x.append(a.sum(0))
-    return np.array(x)
-
 # These arrays have the same size as the countries array (unique countries)
 totCases=gatherDataByCountry(df=cases,nHeaderCols=4)
 totDeaths=gatherDataByCountry(df=deaths,nHeaderCols=4)
 totRecov=gatherDataByCountry(df=recov,nHeaderCols=4)
 
 # -------------------
-# Ratios by array. Correction in cases there are zeros in the denominators
+# Search regions to illustrate the case-fatality ratios
 # -------------------
-def correctedArrayRatio(a,b):
-    bCorr= b.copy()
-    bCorr[bCorr==0]=1
-    return a/np.maximum(b,bCorr)
+Pops_Millions = {'China':1439323776, 'Japan':126476461,'Korea, South':51269185, 'Indonesia':273523615, 'Singapore':5850342, 'Mexico':128932753,
+'US':331002651, 'Canada':37742154, 'Argentina':45195774, 'Brazil':212559417, 'Colombia':50882891, 'Niger':24206644, 'Algeria':43851044, 'Egypt':102334404, 'South Africa':59308690,'Spain':46754778,'Italy':60461826,'France':67886011,'Germany':83783942, 'Australia':25499884,'United Kingdom':67886011,'Iran':83992949,'Israel':8655535}
+#
+R1=['China','Japan','Korea, South','Indonesia','Singapore','Australia']
+R2=['United Kingdom','Spain','Italy','France']
+#America=['Mexico','US','Argentina','Brazil','Colombia','Chile']
+LatinAmerica=['Mexico','Argentina','Brazil','Colombia']
+Africa=['Niger','Algeria','Egypt','South Africa']
+R3=['US','Canada','Germany']
+R4 =['Iran','Israel']
+#MiddleEast =['Iran','Lebanon', 'West Bank and Gaza','Israel']
+regions=[R1,LatinAmerica+Africa,R3+R4,R2]
+#
+ii = getIndsRegions(countries, regions)
+#
+def plotCasesDeathsTS(casesTS,deathsTS,regions,countries, convFactor=1000):
+    ii = getIndsRegions(countries, regions)
+    figu= gr.figure(figsize=(15,9))
+    figu.suptitle('''Deaths vs cases per %d habitants between %s-%s'''%(convFactor,dates[0],dates[-1]))
+    gr.ioff(); ax=list(); sax=list(); cols=2
+    rows = np.int32(np.ceil(len(regions)/cols))
+    ticks= np.arange(0,nDays,7)
+    for n in range(len(regions)):
+        ax.append(figu.add_subplot(rows,cols,n+1))
+        sax.append(inset_axes(parent_axes=ax[n],
+                                width="30%", # width = 30% of parent_bbox
+                                height="30%", # height : 1 inch
+                                loc='lower right'))
+        region=ii[n]
+        for nn in range(len(region)):
+            cas=convFactor*casesTS[region[nn]]/Pops_Millions[countries[region[nn]]]
+            dea=convFactor*deathsTS[region[nn]]/Pops_Millions[countries[region[nn]]]
+            ax[n].plot(cas,dea,'-',label=countries[region[nn]])
+            ax[n].set_xlabel(r'cases per %d habitants'%convFactor)
+            ax[n].set_ylabel(r'deaths per %d habitants'%convFactor)
+            sax[n].plot(cas,dea,'-',label=countries[region[nn]])
+        ymm= ax[n].get_ylim()[1]/3
+        xmm= ax[n].get_xlim()[1]/2
+        sax[n].set_ylim(0.0,ymm);  sax[n].set_xlim(0.0,xmm); sax[n].set_xticklabels([])
+        ax[n].legend(ncol=6,loc='upper left',fontsize=8)
+    figu.subplots_adjust(left=0.075,bottom=0.075,right=0.97,top=0.95,wspace=0.2,hspace=0.25)
+    gr.ion(); gr.draw(); gr.show()
+    return figu
 
+figu=plotCasesDeathsTS(casesTS=totCases,deathsTS=totDeaths,regions=regions,countries=countries)
 
 # -------------------
 # Case-Fatality ratio analysis.
 # Total cases
 # -------------------
-cfrAnalysis="""The case fatality ratio is an approximation for the probability of death among cases in an epidemic.
-In fact, it is an upper bound for the proportion of deaths due to infection, assuming that people that have not been confirmed do not have a higher probability of dying because of the infection"""
+cfrAnalysis= '''The case fatality ratio is an approximation for the probability of death among cases in an epidemic. In fact, it is an upper bound for the proportion of deaths due to infection, assuming that people that have not been confirmed do not have a higher probability of dying because of the infection'''
 print(cfrAnalysis)
-"""
+#
+
 cfr= correctedArrayRatio(totDeaths,totCases)
-# Search regions to illustrate the case-fatality ratios
-Asia=['China','Japan','Indonesia','Vietnam']
-Oceania=['Australia','New Zealand']
-Northamerica=['Mexico','US','Canada']
-Southamerica=['Argentina','Brazil','Colombia','Guatemala']
-Africa=['Niger','Algeria','South Africa']
-Europe=['Spain','Italy','Germany','United Kingdom']
-regions=[Asia,Oceania,Northamerica,Southamerica,Africa,Europe]
-# Indexing and gathering of data
+#
+regions=[R1,LatinAmerica+Africa,R3+R4,R2]
+#
+def plotCFRTS(cfr,dates,regions,countries, convFactor=1000):
+    ii = getIndsRegions(countries, regions)
+    figu = gr.figure(figsize=(15,9))
+    figu.suptitle('Percentage of dead/confirmed per %d habitants between %s-%s'''%(convFactor,dates[0],dates[-1]))
+    ax=list(); gr.ioff()
+    cols=2
+    rows = np.int32(np.ceil(len(regions)/cols))
+    ticks= np.arange(0,nDays,7)
+    for n in range(len(regions)):
+        ax.append(figu.add_subplot(rows,cols,n+1))
+        j=ii[n]
+        for nn in range(len(j)):
+            ax[n].plot(100*cfr[j[nn]],'-',label=countries[j[nn]])
+            #ax[n].plot(100*cfr[j[nn]],'wo',ms=2,mec='k',mfc='w')
+        ax[n].set_ylim(0,15)
+        ax[n].legend(ncol=5,loc='upper left',fontsize=8)
+        ax[n].set_xticks(ticks)
+        ax[n].set_xticklabels(dates[ticks],{'fontsize':8})
+        for label in ax[n].get_xticklabels():
+            label.set_rotation(0)
+            label.set_horizontalalignment('center')
+            label.set_fontsize(8)
+    figu.subplots_adjust(left=0.075,bottom=0.075,right=0.97,top=0.95,wspace=0.2,hspace=0.25)
+    gr.ion(); gr.draw(); gr.show()
+    return figu
 
-def getIndsRegion(region):
-    i = [np.where(countries==region[nn])[0][0] for nn in range(len(region))]
-    return i
-
-def getIndsRegions(regions):
-    ii =list()
-    for mm in range(len(regions)):
-        reg = regions[mm]
-        ii.append(getIndsRegion(reg))
-    return ii
-
-ii = getIndsRegions(regions)
-
-for n in range(rows):
-    j = ii[n]
-    for nn in range(len(ii[n])):
-        print(cfr[j[nn]])
-
-figu = gr.figure(figsize=(15,9))
-ax=list(); gr.ioff()
-rows=len(regions)
-ticks=np.arange(0,nDays,7)
-for n in range(rows):
-    ax.append(figu.add_subplot(rows,1,n+1))
-    j=ii[n]
-    for nn in range(len(j)):
-        ax[n].plot(cfr[j[nn]],'-',label=countries[j[nn]])
-        ax[n].plot(cfr[j[nn]],'wo',ms=2,mec='k',mfc='w')
-    ax[n].legend(ncol=5,loc='upper left',fontsize=8)
-    ax[n].set_xticks(ticks)
-    ax[n].set_xticklabels(dates[ticks],{'fontsize':8})
-    for label in ax[n].get_xticklabels():
-        label.set_rotation(0)
-        label.set_horizontalalignment('center')
-        label.set_fontsize(8)
-figu.subplots_adjust(left=0.1,bottom=0.1,right=0.97,top=0.97,wspace=0.2,hspace=0.25)
-gr.ion(); gr.draw(); gr.show()
-
-
+plotCFRTS(cfr,dates,regions,countries, convFactor=1000)
 # ---------------------------------------------
-# ---------------------------------------------
-# ---------------------------------------------
-
-
-
-
-
-
-uCountries = np.unique(countries)
-nCountries= len(uCountries)
-tCases= list()
-tDeaths= list()
-#tRecov= list()
-for n in range(nCountries):
-    totCases,totDeaths= getCountryTotals(uCountries[n],countries,c,d)
-    tCases.append(totCases)
-    tDeaths.append(totDeaths)
-    #tRecov.append(totRecov)
-
-tCases=np.array(tCases)
-tDeaths=np.array(tDeaths)
-
-def correctedArrayRatio(a,b):
-    bCorr= b.copy()
-    bCorr[bCorr==0]=1
-    return a/np.maximum(b,bCorr)
-
-def totalCFRCRR(country,countries):
-    iCountry = np.where(countries==country)[0]
-    tc = c[iCountry].sum(0)
-    td = d[iCountry].sum(0)
-    tr = r[iCountry].sum(0)
-    cfr = correctedArrayRatio(totDeaths,totCases)
-    crr = correctedArrayRatio(totRecov,totCases)
-    tot = {'cases':tc,'deaths':td,'decov':tr,
-    'cfr':cfr,'crr':crr,'indices':iCountry}
-    return tot
-
-
-
-# -----------------------------------
-# China analysis only
-# -----------------------------------
-totChina= getCountryTotals(countries,'China')
-iChina = np.where(countries=='China')[0]
-
-f=gr.figure(figsize=(17,9)); gr.ioff()
-for n in iChina:
-    gr.plot( 100*cfr[n],lw=1, alpha=0.5, label=provinces[n])
-gr.plot(100*cfr.mean(0),'r', alpha=0.8, lw=2, label='avg provinces')
-gr.plot(100*totChina['cfr'],'k', lw=3, alpha=0.8, label='All China')
-gr.legend(ncol=4,fontsize=8)
-gr.ylim(0,5)
-gr.xlabel('Days from January 22')
-gr.ylabel('% death/conf ')
-gr.ion(); gr.draw(); gr.show()
